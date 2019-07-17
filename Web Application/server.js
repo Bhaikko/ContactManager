d5f9529ca606f5db0065bb502ef7f61b5cc5fe70
Bhaikko <siddhartha.pawar17@gmail.com>
2019-07-17 23:55:57 +0530

    Homepage Added

--- Web Application/server.js
+++ Web Application/server.js
@@ -5,5 +5,10 @@ server.set("view engine", "hbs");
 server.use(express.json());
 server.use(express.urlencoded({extended: true}));
 
+server.use("/", express.static("./public"));
+// server.use("/", express.static("./images"));
+server.use("/login", express.static("./public/login.html"));
+server.use("/aboutUs", express.static("./public/aboutUs.html"));
+
 let port = 4000;
 server.listen(port, () => console.log("Server Up And Running On 127.0.0.1:" + port));
